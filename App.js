09ba17ad986a728969dc5899ce7d5ee6acec0ec9
alenslyn <alenslyn@gmail.com>
2024-03-29 13:50:57 +0100

    feat: add new game button

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import GameScreen from "./screens/GameScreen";
 export default function App() {
   const [userNum, setUserNum] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
@@ -31,6 +32,11 @@ export default function App() {
     setGameIsOver(true);
   }
 
+  function startNewGame() {
+    setUserNum(null);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNum={pickedNumHandler} />;
 
   if (userNum) {
@@ -38,7 +44,13 @@ export default function App() {
   }
 
   if (gameIsOver && userNum) {
-    screen = <GameOverScreen />;
+    screen = (
+      <GameOverScreen
+        userNum={userNum}
+        roundsNum={guessRounds}
+        onStartNewGame={startNewGame}
+      />
+    );
   }
 
   return (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,59 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import Colors from "../constants/colors";
+import Title from "../components/ui/Title";
+import MainButton from "../components/ui/MainButton";
 
-function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+function GameOverScreen({ roundsNum, userNum, onStartNewGame }) {
+  return (
+    <View style={styles.screen}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      </View>
+      <Text style={styles.sumText}>
+        Your phone needed <Text style={styles.highlight}>{roundsNum}</Text>{" "}
+        rounds to guess the number{" "}
+        <Text style={styles.highlight}>{userNum}</Text>.
+      </Text>
+      <MainButton onPress={onStartNewGame}>Start new game</MainButton>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  sumText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
